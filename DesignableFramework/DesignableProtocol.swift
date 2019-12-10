7a85469e227deb2f9912df36053763eee968914e
Nasib Ali <nasib.ali@paytm.com>
2019-12-10 16:02:03 +0530

    Test6

--- DesignableFramework/DesignableProtocol.swift
+++ DesignableFramework/DesignableProtocol.swift
@@ -25,7 +25,7 @@ extension DesignableProtocol where Self: UIView {
             return 1
         }
         set{
-            self.layer.cornerRadius = 2
+            self.layer.cornerRadius = 20
         }
     }
     var border: CGFloat {
